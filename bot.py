89e274430faa0ba96e7de18bab89a2b1f881af0e
Ahmed Jadani <ahmed4star@gmail.com>
2021-10-06 23:22:08 +0200

    adding ternary conditional operator

--- bot.py
+++ bot.py
@@ -32,10 +32,7 @@ async def intra42(ctx):
     URL = "https://cdn.intra.42.fr/users/large_"
     imgex = ".jpg"
     x = len(content.split())
-    if x > 1:
-        y = 1
-    else:
-        y = 0
+    y = 1 if (x > 1) else 0  # using ternery for short and sweet codes 
     msg = arr[y].strip(" ")
     link = URL+msg+imgex
     r = requests.get(link)
